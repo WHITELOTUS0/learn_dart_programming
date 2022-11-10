c8d419769f3ee5bd6951bec47c47500160567f16
WHITELOTUS0 <84666504+WHITELOTUS0@users.noreply.github.com>
2022-11-10 15:05:42 +0300

    first dart files

---
+++ 2_variablles.dart
@@ -0,0 +1,8 @@
+int main() {
+  var name = "SIBO GLORRY";
+  var age = 24;
+  var district = "Kampala";
+
+  print("My name is ${name}, I am ${age}, I come from ${district}");
+  return 0;
+}
---
+++ 3_operators.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  int a = 2;
+  int b = 4;
+  int c = 0;
+
+  c = a * b;
+  print("$a * $b = $c");
+}
