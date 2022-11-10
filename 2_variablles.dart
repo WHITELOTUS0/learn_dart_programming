56dc22f011b3c4698836a329bce418c245b7b0f3
WHITELOTUS0 <84666504+WHITELOTUS0@users.noreply.github.com>
2022-11-10 15:14:41 +0300

    added comment

--- 2_variablles.dart
+++ 2_variablles.dart
@@ -1,7 +1,7 @@
 int main() {
   var name = "SIBO GLORRY";
   var age = 24;
-  var district = "Kampala";
+  var district = "Kampala";//This is a comment
 
   print("My name is ${name}, I am ${age}, I come from ${district}");
   return 0;
