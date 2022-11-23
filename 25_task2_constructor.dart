352170b2063867234c8b4dce0d8dd6b7f8401362
WHITELOTUS0 <84666504+WHITELOTUS0@users.noreply.github.com>
2022-11-23 18:46:42 +0300

    constructors

---
+++ 25_task2_constructor.dart
@@ -0,0 +1,13 @@
+class Car {
+	String make;
+   	String model;
+   	String yearMade;
+   	bool hasABS;
+   
+   	Car(String make, String model, int year, bool hasABS) {
+    	this.make = make;
+      	this.model = model;
+      	this.yearMade = year;
+      	this.hasABS = hasABS;
+   	}
+}
